fe62ed117d46d72a1c8829e00b33951e4772a0d5
Swayambhu <swayambhu43@gmail.com>
2023-01-25 22:51:52 -0500

    tournaments table added

--- backend/database/sql/Table_Creation.sql
+++ backend/database/sql/Table_Creation.sql
@@ -34,3 +34,23 @@ CREATE TABLE tournaments(
     name VARCHAR(100) NOT NULL,
     PRIMARY KEY(id)
 );
+
+CREATE TABLE tournament_details(
+	id INT AUTO_INCREMENT,
+    version INT NOT NULL,
+    street_address VARCHAR(50),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    country VARCHAR(50),
+    host_id INT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    tournament_id INT NOT NULL,
+    CHECK(start_date < end_date),
+    PRIMARY KEY(id),
+    FOREIGN KEY (host_id) REFERENCES clubs_or_organisations(id),
+    FOREIGN KEY (tournament_id) REFERENCES tournaments(id)
+);
+
+
+
